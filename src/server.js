e5500ac5c367d1de079c954681d1018dc4281514
franciniltonsoaresmenzes <fransoaresmenzes@gmail.com>
2023-03-27 21:10:55 -0300

    feat: Conhecendo HTTP status codes

--- src/server.js
+++ src/server.js
@@ -1,7 +1,46 @@
 import http from 'node:http'
 
+// - Criar usuários
+// - Listagem usuários
+// - Edição de usuários
+// - Remoção de usuários
+
+// - HTTP
+//   - Método HTTP
+//   - URL
+
+// GET, POST, PUT, PATCH, DELETE
+
+// GET => Buscar um recurso do back-end
+// POST => Criar um recurso no back-end
+// PUT => Atualizar um recurso no back-end
+// PATCH => Atualizar uma informação específica de um recurso no back-end
+// DELETE => Deletar um recurso do back-end
+
+// GET /users => Buscando usuários no banc-end
+// POST /users => Criar um usuário no back-end
+
+const users = []
+
 const server = http.createServer((req, res) => {
-  return res.end('Hello Word')
+  const { method, url } = req
+
+  if (method === 'GET' && url === '/users') {
+    return res
+      .setHeader('Content-type', 'aplication/json')
+      .end(JSON.stringify(users))
+  }
+
+  if (method === 'POST' && url === '/users') {
+    users.push({
+      id: 1,
+      name: 'Jhon Doe',
+      mail: 'jhondoe@example.com'
+    })
+    return res.writeHead(201).end()
+  }
+
+  return res.writeHead(404).end()
 })
 
 server.listen(3333)
